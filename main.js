b43da45c944c8a5e7f8207674e5a043936e63cdd
Pablogs989 <pablogonsan2001@gmail.com>
2024-03-21 12:04:24 +0100

    Operadores

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Booleanos
+
 const booleano1 = true;
 const booleano2 = false;
 
@@ -9,3 +11,13 @@ const booleanoNot = !booleano2;
 console.log(booleanoAnd);
 const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
 console.log(booleanoMix0);
+
+
+//Operadores
+
+const valorDivision = (17 / 3).toFixed(2);
+console.log(valorDivision);
+const valorResto = 17 % 7
+console.log(valorResto);
+
+
