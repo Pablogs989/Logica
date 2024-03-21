faaefd166fec0b35d01bf13d1149f5883970b0d4
Pablogs989 <pablogonsan2001@gmail.com>
2024-03-21 12:00:10 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const booleano1 = true;
+const booleano2 = false;
+
+const booleanoAnd = booleano1 && booleano2;
+console.log(booleano1);
+const booleanoOr = booleano1 || booleano2;
+console.log(booleano2);
+const booleanoNot = !booleano2;
+console.log(booleanoAnd);
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+console.log(booleanoMix0);
+
